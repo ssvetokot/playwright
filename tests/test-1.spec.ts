ad3e458880a4ed7efdfb2042c3e27eca451b6aa4
ssvetokot <outoftheworld2002@gmail.com>
2026-06-08 16:31:03 +0300

    add tests

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
-});
